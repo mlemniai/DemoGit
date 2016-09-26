1f7589bb3b7bf693751a05c5c348f6f6eddc60c2
hh291029 <hh291029@C39444.ens.uqam.ca>
2016-09-26 09:55:59 -0400

    factorise

---
+++ src/gitdemo/HelloClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author hh291029
+ */
+public class HelloClass {
+    public static void hello(){
+        
+            System.out.println("hello word");
+        
+        
+    }
+    
+}
